9bdd4fa4fda39bd6c7dc124317f7cd49e93c1fda
JeanPiton <jean.orpiton@gmail.com>
2023-09-08 20:47:40 -0300

    Fix: route call function

--- src/routes/passengers.routes.js
+++ src/routes/passengers.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { validateQuerySchema, validateSchema } from "../middlewares/validateSchema.js";
 import { passengerName, passengerSchema } from "../schemas/passengerSchema.js";
-import { createPassenger } from "../controllers/passengers.controllers.js";
+import { createPassenger, getPassengerTravels } from "../controllers/passengers.controllers.js";
 
 const passengerRouter = Router()
 
 passengerRouter.post("/passengers",validateSchema(passengerSchema),createPassenger)
-passengerRouter.get("/passengers/travels",validateQuerySchema(passengerName))
+passengerRouter.get("/passengers/travels",validateQuerySchema(passengerName),getPassengerTravels)
 
 export default passengerRouter
